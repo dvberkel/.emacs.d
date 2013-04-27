c8b1ac28f6bfef047a93b106db19cc1ccc5d6315
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-04-27 09:18:07 +0200

    inform how to describe variables and functions

--- init.el
+++ init.el
@@ -2,20 +2,24 @@
 ;;
 ;; This file loads Org-mode and then loads the rest of our Emacs
 ;; initialization from Emacs lisp embedded in literate Org-mode
-;; files. 
+;; files.
 ;;
 ;; See http://orgmode.org/worg/org-contrib/babel/intro.html
+
+;; You can lookup definitions of variables and functions with the
+;; following functions:
+;; `(describe-variable)` which is bound to C-h v
+;; `(describe-function)` which is bound to C-h f
+;;
+;; For example, when the cursor is over `(describe-function)` and you
+;; perform `C-h f` you are presented with information about
+;; `describe-function`.
+;; This will aid you in the fine points of this init.el file.
 
 (require 'org-install)
 (require 'ob-tangle)
 
-;; Function: file-name-directory filename ; returns the diretory part of filename
-;; Function: buffer-file-name &optional buffer ; return the absolute file name of the file that buffer is visiting
-;; Variable: load-file-name ; hold the name of the file that Emacs is in the process of loading
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name))) ;; "~/.emacs.d/"
-
-;; Function: directory-files directory &optional full-name match-regexp nosort ; returns a list of the names of the ifles in directory
-;; Function: org-babel-load-file file ; load Emacs Lip code blocks embedded in a literate Org-mode file
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
 ;;;; init.el ends here
