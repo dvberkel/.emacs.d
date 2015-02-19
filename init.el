b5cc9f42223e662a58e99267ea09d384f78e4cb0
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-19 09:57:22 +0100

    Remove comments about getting help
    These comments are better served form the org-file

--- init.el
+++ init.el
@@ -6,16 +6,6 @@
 ;;
 ;; See http://orgmode.org/worg/org-contrib/babel/intro.html
 
-;; You can lookup definitions of variables and functions with the
-;; following functions:
-;; `(describe-variable)` which is bound to C-h v
-;; `(describe-function)` which is bound to C-h f
-;;
-;; For example, when the cursor is over `(describe-function)` and you
-;; perform `C-h f` you are presented with information about
-;; `describe-function`.
-;; This will aid you in the fine points of this init.el file.
-
 (require 'org-install)
 (require 'ob-tangle)
 
